d339b38ecc55438a56b7d520026b13c8a1cd8463
Paajanen Santeri <paajanensanteri@gmail.com>
2019-06-06 16:32:44 +0300

    Added selection option for noH
    setup_selection has noH argument with which the sel2 will not be made.

--- src/utils.py
+++ src/utils.py
@@ -65,7 +65,7 @@ def get_CtoH_selections(carbons):
 
 
 
-def setup_selections(u, sel1, sel2, leaflets, leafletatom, thick, thickatom):
+def setup_selections(u, sel1, sel2, leaflets, leafletatom, thick, thickatom, noH):
     # Setup defaults to None
     sellower1 = None; sellower2 = None; thickup = None; thickdown=None
 
@@ -98,41 +98,50 @@ def setup_selections(u, sel1, sel2, leaflets, leafletatom, thick, thickatom):
         for i in range(len(sel1)):
             sel1[i], sellower1[i] = leafletdiv(sel1[i], leafletatom)
 
-
-    if(sel2==[""]):
-        # If second selection isn't give, get the hydrogen
-        sel2 = [None for s in sel1]
-        for i in range(len(sel1)):
-            sel1[i], sel2[i] = get_CtoH_selections(sel1[i])
-        if(leaflets):
-            sellower2 = [None for s in sellower1]
+    if(not noH):
+        if(sel2==[""]):
+            # If second selection isn't give, get the hydrogen
+            sel2 = [None for s in sel1]
             for i in range(len(sel1)):
-                sellower1[i], sellower2[i] = get_CtoH_selections(sellower1[i])
-    else:
-        # Else set it up just like sel1
-        if(isinstance(sel2[0], str)):
-            sel2 = u.select_atoms(sel2[0])
+                sel1[i], sel2[i] = get_CtoH_selections(sel1[i])
+            if(leaflets):
+                sellower2 = [None for s in sellower1]
+                for i in range(len(sel1)):
+                    sellower1[i], sellower2[i] = get_CtoH_selections(sellower1[i])
         else:
-            sel2 = MDAnalysis.AtomGroup(sel2[0], u)
-        if(leaflets):
-            sellower2 = [None]
-            sel2[0], sellower2[0] = leafletdiv(sel2[0], leafletatom)
+            # Else set it up just like sel1
+            if(isinstance(sel2[0], str)):
+                sel2 = u.select_atoms(sel2[0])
+            else:
+                sel2 = MDAnalysis.AtomGroup(sel2[0], u)
+            if(leaflets):
+                sellower2 = [None]
+                sel2[0], sellower2[0] = leafletdiv(sel2[0], leafletatom)
 
 
     print()
 
     # Check the selection sizes
-    for i in range(len(sel1)):
-        if(len(sel1[i].atoms) != len(sel2[i].atoms)):
-            raise ValueError("sel1[%d] and sel2[%d] are different sizes: %d and %d" % (i, i, len(sel1[i].atoms), len(sel2[i].atoms)))
-        if(leaflets and len(sellower1[i].atoms) != len(sellower2[i].atoms)):
-            raise ValueError("sellower1[%d] and sellower2[%d] are different sizes: %d and %d" % (i, i, len(sel1[i].atoms), len(sel2[i].atoms)))
+    if(not noH):
+        for i in range(len(sel1)):
+            if(len(sel1[i].atoms) != len(sel2[i].atoms)):
+                raise ValueError("sel1[%d] and sel2[%d] are different sizes: %d and %d" % (i, i, len(sel1[i].atoms), len(sel2[i].atoms)))
+            if(leaflets and len(sellower1[i].atoms) != len(sellower2[i].atoms)):
+                raise ValueError("sellower1[%d] and sellower2[%d] are different sizes: %d and %d" % (i, i, len(sellower1[i].atoms), len(sellower2[i].atoms)))
+    else:
+        for i, sel in enumerate(sel1):
+            if(len(sel)!=len(sel1[0])):
+                raise ValueError("-noH=True, but sel1[0] and sel1[%d] are different sizes: %d and %d" % (i, len(sel1[0].atoms), len(sel.atoms)))
+
 
     if(leaflets):
         print("Calculating order parameter for selections of %d atoms for upper leaflet"%(np.sum([len(s.atoms) for s in sel1])))
         print("Calculating order parameter for selections of %d atoms for lower leaflet"%(np.sum([len(s.atoms) for s in sellower1])))
     else:
-        print("Calculating order parameter for selections of %d atoms"%(np.sum([len(s.atoms) for s in sel1])))
+        if(noH):
+            print("Calculating order parameter for %d selections of %d atoms"%(len(sel1), len(sel1[0])))
+        else:
+            print("Calculating order parameter for selections of %d atoms"%(np.sum([len(s.atoms) for s in sel1])))
 
     if(thick):
         print("Calculating thickness with a selection of %d atoms for upper leaflet"%(len(thickup.atoms)))
